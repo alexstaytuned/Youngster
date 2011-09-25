e8614188d8160c3db74b4b2149e7ae9c6f4f31cd
Alex Yarmula <oleksiy.yarmula+hosting@gmail.com>
2011-09-25 14:23:34 -0700

    rename another default service

--- src/main/js/default-json.js
+++ src/main/js/default-json.js
@@ -1,7 +1,7 @@
 var data_json = {"services": [
         {"name": "Zookeeper", "version": "1.3.3", "description": "ZooKeeper is a centralized service for maintaining configuration information, naming, providing distributed synchronization, and providing group services. All of these kinds of services are used in some form or another by distributed applications. Each time they are implemented there is a lot of work that goes into fixing the bugs and race conditions that are inevitable.", "color": "yellow", "dependencies": []},
-        {"name": "MySQL", "version": "5.1.46.8", "description": "MySQL is a relational database management system (RDBMS) that runs as a server providing multi-user access to a number of databases.", "color": "green", "dependencies":["mysql_status"]},
-        {"name": "mysql_status", "version": "1.2.3", "description": "This system provides an extensible way to both collect MySQL statistics/status and related information, as well as to serve it as a webservice for use with Vips or Yfor rotation.  (GSLB rotations not generally recommended).", "color": "blue", "dependencies":[]},
+        {"name": "MySQL", "version": "5.1.46.8", "description": "MySQL is a relational database management system (RDBMS) that runs as a server providing multi-user access to a number of databases.", "color": "green", "dependencies":["NFS"]},
+        {"name": "NFS", "version": "n/a", "description": "MySQL is writing to an NFS filer", "color": "blue", "dependencies":[]},
         {"name": "Solr", "version": "3.4.0", "description": "Solr is the popular, blazing fast open source enterprise search platform from the Apache Lucene project. Its major features include powerful full-text search, hit highlighting, faceted search, dynamic clustering, database integration, rich document (e.g., Word, PDF) handling, and geospatial search.", "color": "pink", "dependencies":["Zookeeper"]}
     ],
 
@@ -10,7 +10,7 @@ var data_json = {"services": [
 		 "services": [{"name": "Zookeeper", "position_x": 0, "position_y": 2}, {"name": "Solr", "position_x": 2, "position_y": 3}]
 		},
 		{"hostname": "blahblah.hostname2.com",
-		 "services": [{"name": "mysql_status", "position_x": 3, "position_y": 3}, {"name": "MySQL", "position_x": 3, "position_y": 4}]
+		 "services": [{"name": "NFS", "position_x": 3, "position_y": 3}, {"name": "MySQL", "position_x": 3, "position_y": 4}]
 		}
 	]
 };
